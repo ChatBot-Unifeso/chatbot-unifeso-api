82400c3b1d48af0f2d6a5b8203921006c9512a63
arthcc <arthurdaconceicaocunha@gmail.com>
2024-03-15 20:59:17 -0300

    feat: making dto for creating company

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,7 +1,17 @@
 // se baseia no arquivo schema.prisma
 
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateCompanyDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  
+  @IsString()
+  @IsNotEmpty()
   cnpj: string;
+
+  @IsString()
+  @IsNotEmpty()
   slug: string;
 }
